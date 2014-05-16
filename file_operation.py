3409f062c2f9951ddcc1948a81c17f2729f9eedd
Hao <soccerhaotian@hotmail.com>
2014-05-16 18:59:44 -0400

    Update file_operation.py

--- file_operation.py
+++ file_operation.py
@@ -2,6 +2,8 @@ f = open(filename, 'r')
 
 # Read all lines of the file and put it into a array
 lines = f.readlines()
+print lines
 
 #Read all lines of the file and put it into a string
 content = f.read()
+print content,
